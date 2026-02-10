07f1be0a5694a238f9ec72cf50316f5f3294e0cf
Benign X <1341398182@qq.com>
2026-02-10 21:29:55 +0800

    💄(ui): update window size to 1200x800 and rename title

--- apps/filmr/src/ui/mod.rs
+++ apps/filmr/src/ui/mod.rs
@@ -10,12 +10,13 @@ use eframe::egui;
 pub fn run() -> eframe::Result<()> {
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
-            .with_inner_size([1280.0, 720.0])
+            .with_inner_size([1200.0, 800.0])
+            .with_min_inner_size([1200.0, 800.0])
             .with_drag_and_drop(true),
         ..Default::default()
     };
     eframe::run_native(
-        "Filmr GUI",
+        "Filmr - Film makeR",
         options,
         Box::new(|cc| Ok(Box::new(FilmrApp::new(cc)))),
     )
